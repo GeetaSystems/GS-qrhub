3f180eb3d874bc472ee5afc401627cbf9ac05d2f
harshyadav152 <harshyadav.personal01@gmail.com>
2026-09-20 20:45:17 +0530

    branding middleware updated with headers

--- src/middlewares/branding.js
+++ src/middlewares/branding.js
@@ -1,14 +1,3 @@
 'use strict';
-
 const config = require('../config');
-
-/**
- * Inject custom branding headers on every response.
- */
-const brandingHeaders = (req, res, next) => {
-  res.setHeader('X-Powered-By', config.service.vendor);
-  res.setHeader('X-Service',    config.service.name);
-  next();
-};
-
-module.exports = brandingHeaders;
+module.exports = (req,res,next) => { res.setHeader('X-Powered-By',config.service.vendor); res.setHeader('X-Service',config.service.name); next(); };
