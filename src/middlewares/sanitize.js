bcf7a772ab602e58cda999d47e67c40b897a5071
harshyadav152 <harshyadav.personal01@gmail.com>
2026-09-20 20:46:27 +0530

    sanitize input middleware for correct input format

--- src/middlewares/sanitize.js
+++ src/middlewares/sanitize.js
@@ -1,14 +1,3 @@
 'use strict';
-
 const { sanitizeQuery } = require('../helpers/sanitize');
-
-/**
- * Middleware that sanitizes all query string values in-place
- * before they reach controllers or validators.
- */
-const sanitizeInputs = (req, res, next) => {
-  req.query = sanitizeQuery(req.query);
-  next();
-};
-
-module.exports = sanitizeInputs;
+module.exports = (req,res,next) => { req.query = sanitizeQuery(req.query); next(); };
